116e9807bf83f5a02c8acca9b2cf7e0f4f9e2f5b
olenhad <momeriqbal2@gmail.com>
2013-02-17 03:31:53 +0800

    project details

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject ring.middleware.filter-routes "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject ring-filter-routes "0.1.0-SNAPSHOT"
+  :description "simple route filtering middleware"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
