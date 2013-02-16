f77849029df615ec3f519ebffd2dcd1c46911a61
olenhad <momeriqbal2@gmail.com>
2013-02-17 04:26:26 +0800

    added url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ring-filter-routes "0.1.1"
   :description "simple route filtering middleware"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/olenhad/ring-filter-routes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
