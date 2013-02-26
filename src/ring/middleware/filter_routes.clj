72f56cb506ea4011f636073157d9f3a3dc3fcfa2
olenhad <momeriqbal2@gmail.com>
2013-02-26 17:49:33 +0800

    added support for regex urls

--- src/ring/middleware/filter_routes.clj
+++ src/ring/middleware/filter_routes.clj
@@ -6,15 +6,18 @@
      (cond
       (not (nil? acc))
       acc
-      (and (= (req :uri)  (m :url))
-           (not ((m :check))))
+      (and (cond
+            (= (type (m :url)) java.lang.String)
+            (= (req :uri)  (m :url))
+            (= (type (m :url)) java.util.regex.Pattern)
+            (re-find (m :url) (req :uri)))
+           (not ((m :check) req)))
       (m :else-action)
       :else
       nil))
    nil
    filters
    ))
-
 (defn wrap-filter-routes [app filters]
   "middleware for adding filters to routes. filters is a vector of hashmaps containing :url :check and :else-action. :url is the obviously the url which is checked for. :check should be a function that returns a truthy/falsey value. :else-action is executed if :check is NOT satisfied.
  The function passed as :else-action is executed instead of (app req), so make sure you add an appropriate ring response."
